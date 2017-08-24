39d76419a7be620d36cd227eaec8e192c2653487
jadefeng <jade.jiefeng@gmail.com>
2017-08-24 01:16:56 -0700

    Checkpoint 16 - refactoring landing page with jquery

--- 2_module2/bloc-jams/scripts/landing.js
+++ 2_module2/bloc-jams/scripts/landing.js
@@ -1,47 +1,31 @@
  var pointsArray = document.getElementsByClassName('point');
 
- var animatePoints = function(points) {
+ var animatePoints = function() {
 
-     var revealPoint = function(i) {
-        console.log("revealPoint is geting called", points[i])
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-
+     var revealPoint = function() {
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
      }
 
-     var actions =[]
-
-     for (var i = 0; i < points.length; i++) {
-        // actions[i] = function(index) {
-        //     revealPoint(index);
-        // }
-        
-        // setTimeout(function(){ 
-        //     // revealPoint(i); 
-        //     console.log(i);
-        // }, 3000);
-        revealPoint(i);
-     }
-
-     // forEach(points, revealPoint)
+     $.each($('.point'), revealPoint);
  
  };
 
-window.onload = function() { // window.onload is an event handler
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+ $(window).load(function() {
+     if ($(window).height() > 950) {
+         animatePoints();
      }
 
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
 
-     window.addEventListener('scroll', function(event) {
+     $(window).scroll(function(event) {
          // console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
-         }
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
+      }
      });
      // addEventListener has type, listener, useCapture --> what is useCapture?
- }
+ });
